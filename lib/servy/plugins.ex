07490e9de9700fe7d04064d777645ac7a836aced
Francesc Leveque <francesc@leveque.es>
2025-04-08 20:01:19 +0200

    Using structs instead of maps

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -5,36 +5,38 @@ defmodule Servy.Plugins do
 
   require Logger
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  alias Servy.Conv
+
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{ conv | path: "/wildthings" }
   end
 
-  def rewrite_path(%{path: path} = conv) do
+  def rewrite_path(%Conv{path: path} = conv) do
     regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
     captures = Regex.named_captures(regex, path)
     rewrite_path_captures(conv, captures)
   end
 
-  defp rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+  defp rewrite_path_captures(%Conv{} = conv, %{"thing" => thing, "id" => id}) do
     %{ conv | path: "/#{thing}/#{id}" }
   end
 
-  defp rewrite_path_captures(conv, nil), do: conv
+  defp rewrite_path_captures(%Conv{} = conv, nil), do: conv
 
-  def log(conv), do: IO.inspect conv
+  def log(%Conv{} = conv), do: IO.inspect conv
 
   @doc "Logs 404 requests"
-  def track(%{status: 404, path: path} = conv) do
+  def track(%Conv{status: 404, path: path} = conv) do
     Logger.warning "Warning: #{path} was not found"
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def emojify(%{status: 200} = conv) do
+  def emojify(%Conv{status: 200} = conv) do
     emojies = String.duplicate("🎉", 5)
     %{ conv | resp_body: emojies <> "\n" <> conv.resp_body  <> "\n" <> emojies }
   end
 
-  def emojify(conv), do: conv
+  def emojify(%Conv{} = conv), do: conv
 end
