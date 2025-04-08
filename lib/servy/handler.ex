99f3dd13de265a8eb83bc254349787e0c7fee384
Francesc Leveque <francesc@leveque.es>
2025-04-08 20:39:46 +0200

    Parse request using head and tail

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -47,6 +47,10 @@ defmodule Servy.Handler do
     %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!" }
   end
 
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{ conv | status: 201, resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!" }
+  end
+
   def route(%Conv{method: "GET", path: "/about"} = conv) do
     @pages_path
     |> Path.join("about.html")
@@ -151,5 +155,16 @@ Accept: */*
 
 """
 
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+name=Baloo&type=Brown
+"""
 response = Servy.Handler.handle(request)
 IO.puts(response)
