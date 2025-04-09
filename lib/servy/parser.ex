61be3078ba15a756858acfa4321a2ace8c0a8ee6
Francesc Leveque <francesc@leveque.es>
2025-04-09 18:23:40 +0200

    Use recursion to parse headers & parse params only for one content type

--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -12,18 +12,31 @@ defmodule Servy.Parser do
 
     [method, path, _] = String.split(request_line, " ")
 
-    params = parse_params(params_string)
+    headers = parse_headers(header_lines, %{})
+
+    params = parse_params(headers["Content-Type"], params_string)
 
     %Conv{
       method: method,
       path: path,
+      headers: headers,
       params: params
      }
   end
 
-  defp parse_params(params_string) do
+  defp parse_headers([head | tail], headers) do
+    [key, value] = String.split(head, ": ")
+    headers = Map.put(headers, key, value)
+    parse_headers(tail, headers)
+  end
+
+  defp parse_headers([], headers), do: headers
+
+  defp parse_params("application/x-www-form-urlencoded", params_string) do
     params_string
     |> String.trim
     |> URI.decode_query
   end
+
+  defp parse_params(_, _), do: %{}
 end
