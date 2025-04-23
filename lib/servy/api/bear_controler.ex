8e4ff1014bc391e1911dcb964eaa7d14c59a8c38
Francesc Leveque <francesc@leveque.es>
2025-04-23 21:11:58 +0200

    Section 19 exercises, json. Refactoring response headers

--- lib/servy/api/bear_controler.ex
+++ lib/servy/api/bear_controler.ex
@@ -5,6 +5,13 @@ defmodule Servy.Api.BearController do
     json = Servy.Wildthings.list_bears()
     |> Poison.encode!
 
-    %{conv | status: 200, resp_content_type: "application/json", resp_body: json}
+    conv = put_resp_content_type(conv, "application/json")
+
+    %{ conv | status: 200, resp_body: json }
+  end
+
+  defp put_resp_content_type(%Conv{} = conv, content_type) do
+    new_headers = Map.put(conv.resp_headers, "Content-Type", content_type)
+    %{conv | resp_headers: new_headers}
   end
 end
--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -22,6 +22,7 @@ defmodule Servy.Handler do
     |> route
     |> track
     |> emojify
+    |> put_content_length
     |> format_response
   end
 
@@ -76,11 +77,16 @@ defmodule Servy.Handler do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
+  def put_content_length(%Conv{} = conv) do
+    new_headers = Map.put(conv.resp_headers, "Content-Length", byte_size(conv.resp_body))
+    %{conv | resp_headers: new_headers}
+  end
+
   def format_response(%Conv{} = conv) do
     """
     HTTP/1.1 #{Conv.full_status(conv)}\r
-    Content-Type: #{conv.resp_content_type}\r
-    Content-Length: #{byte_size(conv.resp_body)}\r
+    Content-Type: #{conv.resp_headers["Content-Type"]}\r
+    Content-Length: #{conv.resp_headers["Content-Length"]}\r
     \r
     #{conv.resp_body}
     """
