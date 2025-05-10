6d7da0b98d6b404286700464312778d33b9cb548
Francesc Leveque <francesc@leveque.es>
2025-05-10 12:57:14 +0200

    Section 22. Send & receive messages

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -12,6 +12,7 @@ defmodule Servy.Handler do
 
   alias Servy.Conv
   alias Servy.BearController
+  alias Servy.VideoCam
 
   @doc "Transforms the request into a response"
   def handle(request) do
@@ -24,6 +25,22 @@ defmodule Servy.Handler do
     |> emojify
     |> put_content_length
     |> format_response
+  end
+
+  def route(%Conv{ method: "GET", path: "/snapshots" } = conv) do
+    parent_pid = self()
+
+    spawn(fn -> send(parent_pid, {:result, VideoCam.get_snapshot("cam-1")} end)
+    spawn(fn -> send(parent_pid, {:result, VideoCam.get_snapshot("cam-2")} end)
+    spawn(fn -> send(parent_pid, {:result, VideoCam.get_snapshot("cam-3")} end)
+
+    snapshot1 = receive do {:result, snapshot} -> snapshot end
+    snapshot2 = receive do {:result, snapshot} -> snapshot end
+    snapshot3 = receive do {:result, snapshot} -> snapshot end
+
+    snapshots = [snapshot1, snapshot2, snapshot3]
+
+    %{ conv | status: 200, resp_body: inspect snapshots}
   end
 
   def route(%Conv{ method: "GET", path: "/kaboom" } = _conv) do
