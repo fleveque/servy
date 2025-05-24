7f9df04d1b53e85662c52978601b976c0fb149ee
Francesc Leveque <francesc@leveque.es>
2025-05-24 10:02:11 +0200

    Section 24. Stateful Server Processes part 3 (I)

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -6,7 +6,8 @@ defmodule Servy.PledgeServer do
     receive do
       {:create_pledge, name, amount} ->
         {:ok, id} = send_pledge_to_service(name, amount)
-        new_state = [ {name, amount} | state ]
+        most_recent_pledges = Enum.take(state, 2)
+        new_state = [ {name, amount} | most_recent_pledges ]
         IO.puts "#{name} pledged #{amount} with ID: #{id}"
         IO.puts "New state: #{inspect new_state}"
         listen_loop(new_state)
@@ -21,21 +22,30 @@ defmodule Servy.PledgeServer do
     end
   end
 
-  # def create_pledge(name, amount) do
-  #   {:ok, id} = send_pledge_to_service(name, amount)
+  def create_pledge(pid, name, amount) do
+    send pid, {:create_pledge, name, amount}
+  end
 
-  #   IO.puts("Pledge created with ID: #{id}")
-  # end
+  def recent_pledges(pid) do
+    send pid, {self(), :recent_pledges}
 
-  # def recent_pledges do
-  #   [
-  #     %{name: "Alice", amount: 100},
-  #     %{name: "Bob", amount: 200}
-  #   ]
-  # end
+    receive do {:response, pledges} -> pledges end
+  end
 
   defp send_pledge_to_service(_name, _amount) do
     # Simulate sending the pledge to an external service
     {:ok, "pledge-#{:rand.uniform(1000)}"}
   end
 end
+
+alias Servy.PledgeServer
+
+pid = spawn(PledgeServer, :listen_loop, [[]])
+
+PledgeServer.create_pledge pid, "larry", 10
+PledgeServer.create_pledge pid, "moe", 20
+PledgeServer.create_pledge pid, "curly", 30
+PledgeServer.create_pledge pid, "daisy", 40
+PledgeServer.create_pledge pid, "grace", 50
+
+IO.inspect PledgeServer.recent_pledges pid
