566ef5a54151ffaa1080895c4e6e1f3ba3728235
Francesc Leveque <francesc@leveque.es>
2025-05-29 18:37:14 +0200

    Section 28. Linking processes. KickStarter GenServer linked to HttpServer

---
+++ lib/servy/kick_starter.ex
@@ -0,0 +1,30 @@
+defmodule Servy.KickStarter do
+
+  use GenServer
+
+  def start do
+    IO.puts "Starting Servy KickStarter..."
+    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  # Server Callbacks
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    server_pid = start_server()
+    {:ok, server_pid}
+  end
+
+  def handle_info({:EXIT, _pid, reason}, _state) do
+    IO.puts "HTTP server exited with reason: #{inspect(reason)}"
+    server_pid = start_server()
+    {:noreply, server_pid}
+  end
+
+  defp start_server do
+    IO.puts "Starting the HTTP server..."
+    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    Process.register(server_pid, :http_server)
+    server_pid
+  end
+end
